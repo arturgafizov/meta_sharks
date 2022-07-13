f91099974fd0c797407821f43f37659a70284a56
Artur <art05xxx@rambler.ru>
2022-07-13 17:39:36 +0300

    2 first

--- app/rudn/source/settings.py
+++ app/rudn/source/settings.py
@@ -123,6 +123,7 @@ WSGI_APPLICATION = 'source.wsgi.application'
 
 DATABASES = {
     "default": {
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
